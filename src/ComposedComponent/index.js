eb6d94d1d7a8d0e0ace4547eb195b9f43f8abe81
Ioannis Theodosiou <ioannis@info.nl>
2020-01-14 16:57:41 +0100

    fix list

--- src/ComposedComponent/index.js
+++ src/ComposedComponent/index.js
@@ -1,16 +1,16 @@
-import React, { useReducer } from 'react';
-import { onChangeReducer, CHANGE, initial } from '../shared';
-import * as test from './Tester';
-import SwitchTest from './TestSwitch';
+import React, { useReducer } from "react";
+import { onChangeReducer, CHANGE, initial } from "../shared";
+import * as test from "./Tester";
+import SwitchTest from "./TestSwitch";
 
-const ADD = 'ADD';
-const REMOVE = 'REMOVE';
+const ADD = "ADD";
+const REMOVE = "REMOVE";
 
 const updateListReducer = (state, { type, payload }) => {
   const { list } = state;
   if (type === ADD) {
     list.push(payload);
-    return { ...state, list, value: '' };
+    return { ...state, list, value: "" };
   } else if (type === REMOVE) {
     list.pop();
     return { ...state, list };
@@ -20,7 +20,7 @@ const updateListReducer = (state, { type, payload }) => {
 const reducerHandler = (state, action) => {
   if (action.type.startsWith("C")) {
     return onChangeReducer(state, action);
-  } 
+  }
   return updateListReducer(state, action);
 };
 
@@ -28,14 +28,30 @@ initial.list = [];
 
 const ComposedComponent = () => {
   const [{ value, list }, dispatch] = useReducer(reducerHandler, initial);
-  const Test = test[list[0]];
+
   return (
     <div>
       <div>Input: {value}</div>
-      <input value={value} onChange={({target: {value}}) => dispatch({type: CHANGE, payload: value})} />
-      <button type="button" onClick={() => dispatch({ type: ADD, payload: value })}>+</button>
-      <button type="button" onClick={() => dispatch({ type: REMOVE })}>-</button>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>{!!Test && <SwitchTest at={list[0]} />}{!!Test && <Test />}</div>
+      <input
+        value={value}
+        onChange={({ target: { value } }) =>
+          dispatch({ type: CHANGE, payload: value })
+        }
+      />
+      <button
+        type="button"
+        onClick={() => dispatch({ type: ADD, payload: value })}
+      >
+        +
+      </button>
+      <button type="button" onClick={() => dispatch({ type: REMOVE })}>
+        -
+      </button>
+      <div>
+        {list.map((item, i) => (
+          <span style={{display: 'block'}} key={`item-${i}`}>{item}</span>
+        ))}
+      </div>
     </div>
   );
 };
